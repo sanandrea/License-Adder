e18fc33145d7909e9f9b09d7c99c7e08425ec647
Andi Palo <sanandrea8080@gmail.com>
2016-03-28 13:58:53 +0200

    added readme, small improvements

---
+++ README.md
@@ -0,0 +1,24 @@
+#License Adder
+This simple bash scripts adds a license information to all files
+present in a certain folder. This is useful for old projects
+which do not contain a header informotion in each file.
+
+The script supports only adding GPL and MIT license.
+See add_license.sh at line 52 too add other types of licences.
+
+The supported file extensions for the moment are:
+"c" "h" "cpp" "hpp" "m" "java" "js" "py" "sh"
+
+To add other extensions you can add them in the arrays:
+`slashComments` or `hashComments` 
+
+##Usage
+`./add_license.sh folder_of_project license_header
+
+I have provided two license headers for gplv2 and MIT respectively the files:
+`lcs_mit` and `lcs_gplv2`
+
+The log of all operations is written in a logfile at folder_of_project/project_name.addlcs.log
+
+##Notes
+.git and node_modules folders are excluded from find command.
--- add_license.sh
+++ add_license.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+#
+#  Copyright © 2016 Andi Palo
+#  This file is part of project: License Adder
+#
+
 # script to copy the headers to all the source files and header files
 
 
@@ -23,11 +46,12 @@ read project
 slashComments=("c" "h" "cpp" "hpp" "m" "java" "js")
 hashComments=("py" "sh")
 
-FILES=$(find $1 -type f -not -path "$1/.git/*")
-echo $FILES
+FILES=$(find $1 -type f -not -path "$1/.git/*" -not -path "$1/node_modules/*")
+
+log=$1/$project.add_lcs.log
 for f in $FILES; do
   if (grep -Eq '(?:PURPOSE AND NONINFRINGEMENT|GNU General Public License)' $f);then 
-    echo "No need to copy the License Header to $f"
+    echo "No need to copy the License Header to $f" >> $log
   else
     #get file extension
     #see http://stackoverflow.com/a/965072/1073786
@@ -52,10 +76,11 @@ for f in $FILES; do
         echo $comment >> $f.new
         cat $f >> $f.new
         mv $f.new $f
-        echo "License Header copied to $f"
+        echo "License Header copied to $f" >> $log
     else
-        echo "$f does not belong to licensable files"
+        echo "$f does not belong to licensable files" >> $log
     fi
 
   fi 
 done
+echo "output has been written to: $log"
